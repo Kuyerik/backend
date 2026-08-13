5dcb3b6b5bc070e71a42719841f3d78f897b1c73
Kuyeri Chakupadedza Kuyeri <kuyerikuyeri@gmail.com>
2026-08-13 13:22:53 +0200

    feat: implement products controller and service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  private readonly filePath = path.resolve(process.cwd(), 'src/products.json');
+
+  private getProductsFromFile(): any[] {
+    const fileData = fs.readFileSync(this.filePath, 'utf-8');
+    return JSON.parse(fileData);
+  }
+
+  findAll() {
+    return this.getProductsFromFile();
+  }
+
+  findOne(id: string) {
+    const products = this.getProductsFromFile();
+    const product = products.find((p) => p.id === id);
+    if (!product) {
+      throw new NotFoundException(`Produto com ID ${id} não encontrado`);
+    }
+    return product;
+  }
+}
