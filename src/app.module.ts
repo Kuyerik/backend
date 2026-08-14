50f96c5b578ad2642ff19f34c5d619dd571e0a71
Kuyeri Chakupadedza Kuyeri <kuyerikuyeri@gmail.com>
2026-08-14 02:54:57 +0200

    feat: add ServeStaticModule and update product details with new images and descriptions

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path/win32';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public')
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
